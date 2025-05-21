0ea349cb4467d07f85eae44f3c96ebe771db498a
Michał Marcinkowski <michalmarcinkowski@laptop-Micha.local>
2025-05-21 21:44:26 +0200

    Initial add product endpoint implementation with tests

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use App\Request\ProductCreateRequest;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class ProductController extends AbstractController
+{
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
+
+    public function __construct(SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+    }
+
+    #[Route('/api/products', name: 'app_product_create', methods: ['POST'])]
+    public function create(Request $request): JsonResponse
+    {
+        /** @var ProductCreateRequest $productCreateRequest */
+        $productCreateRequest = $this->serializer->deserialize(
+            $request->getContent(),
+            ProductCreateRequest::class,
+            'json'
+        );
+
+        $violations = $this->validator->validate($productCreateRequest);
+        if (count($violations) > 0) {
+            $errors = [];
+            foreach ($violations as $error) {
+                $errors[$error->getPropertyPath()][] = $error->getMessage();
+            }
+
+            return new JsonResponse([
+                'message' => 'Validation Failed',
+                'errors' => $errors
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return new JsonResponse([
+            'id' => 1,
+            'title' => $productCreateRequest->title,
+            'price' => $productCreateRequest->price,
+        ],
+            Response::HTTP_CREATED,
+            [
+                'Content-Type' => 'application/json; charset=utf-8',
+#                'Location' => '/api/products/1',
+        ]);
+    }
+}
